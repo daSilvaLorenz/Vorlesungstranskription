8c371e66eb730bdae1503de4ef27ac26af528a57
daSilvaLorenz <lukasdasilva@hotmail.de>
2024-05-26 15:43:56 +0200

    #

--- config.py
+++ config.py
@@ -8,5 +8,7 @@ transcribed_audio_folder = "C:/Users/Lukas/Documents/Uni/Würzburg/KI-Tutor/Whis
 original_language_folder = "C:/Users/Lukas/Documents/Uni/Würzburg/KI-Tutor/Whisper/Audiodateien/Testungen/Transkriptionen Deutsch"
 # Ordner für die Englische Übersetzung
 eng_transcript_base = "C:/Users/Lukas/Documents/Uni/Würzburg/KI-Tutor/Whisper/Audiodateien/Testungen/Transkriptionen Englisch"
-# Dateipfad für den API-Schlüssel
+# Dateipfad/Environment für den  Deepl-API-Schlüssel
 api_key = os.getenv("Deepl_API-Key")
+# Dateipfad/Environment für den  Deepl-API-Schlüssel
+whisper_api_key = os.getenv("Whisper_API-Key")
